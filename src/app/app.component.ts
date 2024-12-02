764e3820eea89d3a23832ec43226bdef3f93118b
Sebastian-Rothe <sebi1995@gmx.at>
2024-12-02 16:37:29 +0100

    more like the old version - copyed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';
+import { RouterLink, RouterOutlet,  } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDrawer, MatDrawerContainer } from '@angular/material/sidenav';
+// import { UserComponent } from './user/user.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, MatButtonModule, MatDrawer, MatDrawerContainer, MatIconModule, MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'basti-crm';
