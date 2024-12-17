170eacdc881a67cf31a4cc3a3a1f909bc05ac4fd
Sebastian-Rothe <sebi1995@gmx.at>
2024-12-17 22:11:02 +0100

    change in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 export const routes: Routes = [
-    { path: '', component: DashboardComponent},
+    { path: '', component: FrontpageComponent},
     { path: 'dashboard', component: DashboardComponent},
     { path: 'user', component: UserComponent},
     { path: 'user/:id', component: UserDetailComponent}
