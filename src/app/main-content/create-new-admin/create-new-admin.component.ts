1eeea94fb8207adbfcce107ae64653b8503b7eea
Sebastian-Rothe <sebi1995@gmx.at>
2024-12-31 13:14:05 +0100

    create new admin works now

--- src/app/main-content/create-new-admin/create-new-admin.component.ts
+++ src/app/main-content/create-new-admin/create-new-admin.component.ts
@@ -1,5 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatOption } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -7,47 +13,60 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { FirebaseAuthService } from '../../../services/firebase-auth.service';
+import { User } from '../../../models/user.class';
+import { FirebaseService } from '../../../services/firebase.service';
 @Component({
   selector: 'app-create-new-admin',
   standalone: true,
-  imports: [  MatDialogModule,
-      // MatDialogClose,
-      MatFormFieldModule,
-      FormsModule,
-      MatOption,
-      MatSelectModule,
-      MatInputModule,
-      MatDatepickerModule,
-      MatButtonModule,
-      ReactiveFormsModule],
+  imports: [
+    MatDialogModule,
+    // MatDialogClose,
+    MatFormFieldModule,
+    FormsModule,
+    MatOption,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './create-new-admin.component.html',
-  styleUrl: './create-new-admin.component.scss'
+  styleUrl: './create-new-admin.component.scss',
 })
 export class CreateNewAdminComponent {
   // formGroup: FormGroup;
+  user = new User();
   roles: string[] = ['user', 'admin', 'support', 'management'];
   private _formBuilder = inject(FormBuilder);
-  constructor() {}
+  constructor(
+    private firebaseAuthService: FirebaseAuthService,
+    private firebaseService: FirebaseService
+  ) {}
 
-    formGroup = this._formBuilder.group({
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required],
-        role: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
-          ]
+  formGroup = this._formBuilder.group(
+    {
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      role: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+          ),
         ],
-        confirmPassword: ['', Validators.required]
-      },
-      { validators: this.passwordMatchValidator }
-    );
-  
+      ],
+      confirmPassword: ['', Validators.required],
+    },
+    { validators: this.passwordMatchValidator }
+  );
 
-  passwordMatchValidator(group: FormGroup): { passwordMismatch: boolean } | null {
+  passwordMatchValidator(
+    group: FormGroup
+  ): { passwordMismatch: boolean } | null {
     const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
     return password === confirmPassword ? null : { passwordMismatch: true };
@@ -61,9 +80,45 @@ export class CreateNewAdminComponent {
   }
 
   saveNewAdmin(): void {
-    if (this.formGroup.valid) {
-      console.log(this.formGroup.value);
-      // Add your form submission logic here
-    }
+    const email = this.formGroup.get('email')?.value || '';
+    const password = this.formGroup.get('password')?.value || '';
+
+    this.firebaseAuthService
+      .register(email, password)
+      .then((firebaseUser) => {
+        if (firebaseUser) {
+          // Zusätzliche Daten für Firestore-User setzen
+          this.user.uid = firebaseUser.uid; // UID aus Firebase Auth als ID für Firestore-Dokument
+          this.user.email = firebaseUser.email || '';
+          this.user.firstName = this.formGroup.get('firstName')?.value || '';
+          this.user.lastName = this.formGroup.get('lastName')?.value || '';
+          this.user.role = this.formGroup.get('role')?.value || '';
+          // Benutzer in Firestore speichern
+          this.firebaseService
+            .addUser(this.user)
+            .then(() => {
+              console.log('Benutzer erfolgreich gespeichert:', this.user.uid);
+              // // this.router.navigate(['/']);
+              //       // Dialog öffnen
+              //       const dialogRef = this.dialog.open(SuccessDialogComponent);
+
+              //       // Dialog-Aktion abfangen
+              //       dialogRef.afterClosed().subscribe((result) => {
+              //         if (result === 'goToHome') {
+              //           this.router.navigate(['/']);
+              //         }
+              //       });
+            })
+            .catch((error) => {
+              console.error(
+                'Fehler beim Speichern des Benutzers in Firestore:',
+                error
+              );
+            });
+        }
+      })
+      .catch((error) => {
+        console.error('Fehler beim Registrieren des Benutzers:', error);
+      });
   }
 }
