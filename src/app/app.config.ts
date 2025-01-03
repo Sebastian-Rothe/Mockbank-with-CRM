a4aff9a763463678751cc0585e916501c94e8712
Sebastian-Rothe <sebi1995@gmx.at>
2025-01-03 18:33:17 +0100

    dashboard button

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAuth(() => getAuth()),
