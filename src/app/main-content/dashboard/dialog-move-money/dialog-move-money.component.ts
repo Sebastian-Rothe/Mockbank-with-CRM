8f7c74745ea46d36fd9534a127eb0533238ecc47
Sebastian-Rothe <sebi1995@gmx.at>
2025-01-04 22:39:47 +0100

    begin send form

--- src/app/main-content/dashboard/dialog-move-money/dialog-move-money.component.ts
+++ src/app/main-content/dashboard/dialog-move-money/dialog-move-money.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOption } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-dialog-move-money',
   standalone: true,
-  imports: [],
+  imports: [   MatDialogModule,
+    // MatDialogClose,
+    MatFormFieldModule,
+    MatOption,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    FormsModule],
   templateUrl: './dialog-move-money.component.html',
   styleUrl: './dialog-move-money.component.scss'
 })
--- src/app/main-content/dashboard/dialog-open-new-pocket/dialog-open-new-pocket.component.ts
+++ src/app/main-content/dashboard/dialog-open-new-pocket/dialog-open-new-pocket.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOption } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @Component({
   selector: 'app-dialog-open-new-pocket',
   standalone: true,
-  imports: [],
+  imports: [   MatDialogModule,
+      // MatDialogClose,
+      MatFormFieldModule,
+      MatOption,
+      MatSelectModule,
+      MatInputModule,
+      MatDatepickerModule,
+      MatButtonModule,
+      FormsModule],
   templateUrl: './dialog-open-new-pocket.component.html',
   styleUrl: './dialog-open-new-pocket.component.scss'
 })
