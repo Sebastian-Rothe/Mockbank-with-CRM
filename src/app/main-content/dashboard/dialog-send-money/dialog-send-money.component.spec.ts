5e11301607a13aeddd2cd05ef1cf2d80ff4a8b41
Sebastian-Rothe <sebi1995@gmx.at>
2025-01-04 22:22:21 +0100

    dialog imports added

--- src/app/main-content/dashboard/dialog-send-money/dialog-send-money.component.spec.ts
+++ src/app/main-content/dashboard/dialog-send-money/dialog-send-money.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DialogSentMoneyComponent } from './dialog-sent-money.component';
+import { DialogSentMoneyComponent } from './dialog-send-money.component';
 
 describe('DialogSentMoneyComponent', () => {
   let component: DialogSentMoneyComponent;
---
+++ src/app/main-content/dashboard/dialog-send-money/dialog-send-money.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-sent-money',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-send-money.component.html',
+  styleUrl: './dialog-send-money.component.scss'
+})
+export class DialogSendMoneyComponent {
+
+}
