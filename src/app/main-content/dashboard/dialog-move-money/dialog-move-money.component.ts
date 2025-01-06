448905a67bbcfb418d0eced16c4098540d48886e
Sebastian-Rothe <sebi1995@gmx.at>
2025-01-06 20:46:00 +0100

    working on transfer money

--- src/app/main-content/dashboard/dialog-move-money/dialog-move-money.component.ts
+++ src/app/main-content/dashboard/dialog-move-money/dialog-move-money.component.ts
@@ -7,6 +7,9 @@ import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { Transfer } from '../../../../models/transfer.class';
+import { User } from '../../../../models/user.class';
+import { Account } from '../../../../models/account.class';
 
 @Component({
   selector: 'app-dialog-move-money',
@@ -26,12 +29,18 @@ import { MatSelectModule } from '@angular/material/select';
   styleUrl: './dialog-move-money.component.scss',
 })
 export class DialogMoveMoneyComponent {
-
-
+transfer = new Transfer(); // Neuer Transfer
+// accounts = new Account(); // Neues Konto
+user = new User(); // Neuer Benutzer
   constructor(public dialogRef: MatDialogRef<DialogMoveMoneyComponent>) {} // Methode zum Senden des Geldes
 
-
+moveMoney(): void {
+    // Hier wird der Code zum Senden des Geldes eingefügt
+    this.closeDialog();
+  }
   closeDialog(): void {
     this.dialogRef.close();
   }
 }
+
+//  this c does not know how the user is!!!
--- src/models/account.class.ts
+++ src/models/account.class.ts
@@ -1,3 +1,4 @@
+
 export class Account {
   accountId: string;
   accountName: string = '';
