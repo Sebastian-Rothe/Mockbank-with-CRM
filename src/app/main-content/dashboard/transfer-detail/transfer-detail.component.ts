a4ec9ad7dad681a21f3b0521d41539f480d87365
Sebastian-Rothe <sebi1995@gmx.at>
2025-01-24 17:00:35 +0100

    names added to treansfer

--- src/app/main-content/dashboard/transfer-detail/transfer-detail.component.ts
+++ src/app/main-content/dashboard/transfer-detail/transfer-detail.component.ts
@@ -6,6 +6,9 @@ import {
   MatDialogActions,
   MatDialogContent,
 } from '@angular/material/dialog';
+import { FirebaseService
+
+ } from '../../../../services/firebase.service';
 @Component({
   selector: 'app-transfer-detail',
   standalone: true,
@@ -14,14 +17,37 @@ import {
   styleUrl: './transfer-detail.component.scss',
 })
 export class TransferDetailComponent {
-  transferId: string | null = null;
+
   transfer: Transfer;
+  senderName: string = ''; // Sender-Name
+  receiverName: string = ''; // Empfänger-Name
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Transfer,
-    public dialogRef: MatDialogRef<TransferDetailComponent>
+    public dialogRef: MatDialogRef<TransferDetailComponent>,
+    private firebaseService: FirebaseService
   ) {
     this.transfer = data;
   }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      // Holen der Sender-Daten
+      if (this.transfer.senderUserId) {
+        const sender = await this.firebaseService.getUser(this.transfer.senderUserId);
+        this.senderName = sender ? `${sender.firstName} ${sender.lastName}` : 'Unbekannt';
+      }
+
+      // Holen der Empfänger-Daten
+      if (this.transfer.receiverUserId) {
+        const receiver = await this.firebaseService.getUser(this.transfer.receiverUserId);
+        this.receiverName = receiver ? `${receiver.firstName} ${receiver.lastName}` : 'Unbekannt';
+      }
+    } catch (error) {
+      console.error('Fehler beim Abrufen der User-Daten:', error);
+    }
+  }
+
 
   closeDialog(): void {
     this.dialogRef.close();
