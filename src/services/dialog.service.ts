c48e15fc3f6b69cf3a4f8310f679af36319ae7d1
Sebastian-Rothe <sebi1995@gmx.at>
2025-02-25 19:07:14 +0100

    jsDoc for dialog service

--- src/services/dialog.service.ts
+++ src/services/dialog.service.ts
@@ -8,12 +8,18 @@ import { MessageDialogComponent } from '../dialogs/message-dialog/message-dialog
 export class DialogService {
   constructor(private dialog: MatDialog) {}
 
+  /**
+   * Opens a dialog with a given title and message.
+   * @param {string} title - The title of the dialog.
+   * @param {string} message - The message to display in the dialog.
+   * @returns {Promise<boolean>} A promise that resolves to `true` if the dialog is confirmed, otherwise `false`.
+   */
   openDialog(title: string, message: string): Promise<boolean> {
     const dialogRef = this.dialog.open(MessageDialogComponent, {
       width: '400px',
       data: { title, message },
     });
 
-    return dialogRef.afterClosed().toPromise(); 
+    return dialogRef.afterClosed().toPromise();
   }
 }
