7e5ee561c91fe256556112ece7f8624cb50a4ae8
Sebastian-Rothe <sebi1995@gmx.at>
2025-02-28 11:22:54 +0100

    routing upgrate on frontpage - and scroll to top for links at footer.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,5 +9,7 @@ import { RouterLink } from '@angular/router';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
