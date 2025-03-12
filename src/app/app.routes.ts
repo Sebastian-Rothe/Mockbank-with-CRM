3e90ecf6f204e5ed0648bd6cb029915d850c3917
Sebastian-Rothe <sebi1995@gmx.at>
2025-03-12 13:55:18 +0100

    unused route deleted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,6 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { CreateNewAdminComponent } from './main-content/create-new-admin/create-new-admin.component';
 import { ImprintComponent } from './shared-components/imprint/imprint.component';
 import { FrontpageContentComponent } from './frontpage/frontpage-content/frontpage-content.component';
-import { UserProfileComponent } from './main-content/user-profile/user-profile.component';
 import { ChangeRoleComponent } from './main-content/change-role/change-role.component';
 import { PrivacyPolicyComponent } from './shared-components/privacy-policy/privacy-policy.component';
 import { StatisticsComponent } from './main-content/statistics/statistics.component';
@@ -35,7 +34,6 @@ export const routes: Routes = [
       { path: 'privacy-policy', component: PrivacyPolicyComponent },
       { path: 'help-center', component: HelpCenterComponent },
       { path: 'dashboard', component: DashboardComponent },
-      { path: 'user-profile', component: UserProfileComponent },
       { path: 'user', component: UserComponent },
       { path: 'user/:uid', component: UserDetailComponent },
       { path: 'new-admin', component: CreateNewAdminComponent },
